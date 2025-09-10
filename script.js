6cafa068b2a56a1df79ea5f3e7714a0ea4fc964c
sinbuns <109823000+sinbuns@users.noreply.github.com>
2025-09-10 15:10:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,8 @@ const galleryData = { //upload the images here
     couples: [
         { id: 5, src: './assets/couple1.jpg', title: 'Sinny & Rollback' },
         { id: 6, src: './assets/couple2.png', title: 'Missus & Dark' },
-        { id: 7, src: './assets/image_2025-03-14_235128811.png', title: 'Sinny&Rollback' } 
+        { id: 7, src: './assets/image_2025-03-14_235128811.png', title: 'Sinny&Rollback' },
+        { id: 7, src: './assets/BriNTori.png', title: 'Bri & Tori' } 
     ],
 };
 
@@ -410,4 +411,5 @@ document.addEventListener('click', () => {
         isMusicPlaying = true;
     }
 }, { once: true });
+
 
